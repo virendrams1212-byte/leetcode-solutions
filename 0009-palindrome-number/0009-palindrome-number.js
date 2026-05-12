9927749f6e9ade6bd19b6c8d51bed21ba68e5754
virendra <virendra.ms1212@gmail.com>
2026-05-12 18:10:56 +0530

    Time: 5 ms (78.45%), Space: 63.3 MB (78.44%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.js
+++ 0009-palindrome-number/0009-palindrome-number.js
@@ -3,9 +3,18 @@
  * @return {boolean}
  */
 var isPalindrome = function(x) {
-    let str = x.toString();
-    if(str.split("").reverse().join("") === str)
-    return true;
-    else
-    return false;
+    // negative numbers are not palindrome
+    // numbers ending in 0 cannot be palindrome except 0 itself
+    if (x < 0 || (x % 10 === 0 && x !== 0)) return false;
+
+    let reversedHalf = 0;
+
+    while (x > reversedHalf) {
+        reversedHalf = reversedHalf * 10 + (x % 10);
+        x = Math.floor(x / 10);
+    }
+
+    // even digits: x === reversedHalf
+    // odd digits:  x === Math.floor(reversedHalf / 10)
+    return x === reversedHalf || x === Math.floor(reversedHalf / 10);
 };
